7435842dd221b507f8ed0ad7461af8fce37804f5
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-11 21:14:28 +0200

    0

--- src/components/CodeSnippetPage.tsx
+++ src/components/CodeSnippetPage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getCodeSnippet } from '../api/CodeSnippetApi'
 import { CodeSnippet } from '../types/CodeSnippet'
-import * as svgson from 'svgson' // update import statement
 
 interface Props {
   match: {
@@ -27,25 +26,17 @@ const CodeSnippetPage: React.FC<Props> = ({ match }) => {
     return <div>Loading...</div>
   }
 
-  const svg = svgson.create({ // update function call
-    tag: 'svg',
-    attrs: {
-      xmlns: 'http://www.w3.org/2000/svg',
-      width: 100,
-      height: 30,
-    },
-    children: [
-      {
-        tag: 'text',
-        attrs: {
-          x: 10,
-          y: 20,
-          fontSize: 20,
-        },
-        children: [codeSnippet.code],
-      },
-    ],
-  })
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', '100');
+  svg.setAttribute('height', '30');
+
+  const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+  text.setAttribute('x', '10');
+  text.setAttribute('y', '20');
+  text.setAttribute('font-size', '20');
+  text.textContent = codeSnippet.code;
+
+  svg.appendChild(text);
 
   return (
     <div>
@@ -58,7 +49,7 @@ const CodeSnippetPage: React.FC<Props> = ({ match }) => {
       </div>
       <div>
         <h2>Vector Image</h2>
-        <div dangerouslySetInnerHTML={{ __html: svg }} />
+        <div dangerouslySetInnerHTML={{ __html: svg.outerHTML }} />
       </div>
     </div>
   )
