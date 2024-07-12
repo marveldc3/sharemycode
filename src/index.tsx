4c19667d3a1d922325fe385e3a01ec087b7ffe40
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-12 02:59:34 +0200

    0

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 // src/index.tsx 
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import App from './App' // Make sure you're importing App correctly
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import App from './App'
 import CodeSnippetPage from './components/CodeSnippetPage'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/code-snippets/:id" component={CodeSnippetPage} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/code-snippets/:id" element={<CodeSnippetPage />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
